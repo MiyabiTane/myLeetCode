50a4c3e78cc717c167b264d786d2ddb884d74167
MiyabiTane <miyabitane@yahoo.co.jp>
2019-09-25 17:47:07 +0900

    setの使い方

--- 14_Longest_Common_Prefix.py
+++ 14_Longest_Common_Prefix.py
@@ -1,10 +1,23 @@
 def longestCommonPrefix(strs):
-    if strs==[]:
-        return ""
-    for i in range(len(strs[0])):
-        for string in strs[1:]:
-            if i>=len(string):
-                return strs[0][:i]
-            elif string[i]!=strs[0][i]:
-                return strs[0][:i]
-    return strs[0]
+    result=""
+    i=0
+    while True:
+        try:
+            sets=set(string[i] for string in strs)
+            if len(sets)==1:
+                result+=sets.pop()
+                i+=1
+            else:
+                break
+            #>>> strs=["art","bar","cat"]
+            #>>> for i in range(3):
+            #...     sets=set(string[i] for string in strs)
+            #...     print(sets)
+            #...
+            #set(['a', 'c', 'b'])
+            #set(['a', 'r'])
+            #set(['r', 't'])
+        except Exception as e:
+            break
+
+    return result
