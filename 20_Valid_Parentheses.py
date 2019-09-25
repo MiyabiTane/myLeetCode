c32aeffe5a7bfc42d7984e543ea908f48fa680eb
MiyabiTane <miyabitane@yahoo.co.jp>
2019-09-25 17:52:52 +0900

    Stackを使う

--- 20_Valid_Parentheses.py
+++ 20_Valid_Parentheses.py
@@ -1,25 +1,17 @@
 def isValid(s):
-    left1=0;left2=0;left3=0
+    dict={')':'(','}':'{',']':'['}
+    Stack=[]
     for i in range(len(s)):
-        if s[i]=='(':
-            left1+=1
-        elif s[i]=='{':
-            left2+=1
-        elif s[i]=='[':
-            left3+=1
-        elif s[i]==')':
-            left1-=1
-            if left1<0:
-                return False
-        elif s[i]=='}':
-            left2-=1
-            if left2<0:
-                return False
-        elif s[i]==']':
-            left3-=1
-            if left3<0:
-                return False
-    if left1==0 and left2==0 and left3==0:
+        if Stack==[]:
+            Stack.append(s[i])
+        else:
+            mark=Stack[-1]
+            if dict.get(s[i])==mark:
+                Stack.pop()
+            else:
+                Stack.append(s[i])
+        #print(Stack)
+    if Stack==[]:
         return True
     else:
         return False
