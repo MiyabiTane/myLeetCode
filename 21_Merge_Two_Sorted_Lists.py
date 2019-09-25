32c393f299458ec455d2cd77b4e1db57fb292ec8
MiyabiTane <miyabitane@yahoo.co.jp>
2019-09-25 17:55:39 +0900

    Class NodeList 導入

--- 21_Merge_Two_Sorted_Lists.py
+++ 21_Merge_Two_Sorted_Lists.py
@@ -1,22 +1,42 @@
-class ListNode:
-    def __init__(self,x):
-        self.val=x
-        self.next=None
+# Definition for singly-linked list.
+class ListNode(object):
+     def __init__(self, x):
+         self.val = x
+         self.next = None
 
-class Solution(ListNode):
-    def __init__(self,x):
-        ListNode.__init__(self,x)
-
-    def mergeTwolists(self,l1,l2):
-        head=cur=ListNode(0)
-        while l1!=[] or l2!=[]:
-            if l1==[] or l2.val<l1.val:
-                cur.next=l2
-                l2=l2.next
-            elif l2==[] or l1.val<l2.val:
-                cur.next=l1
-                l1=l1.next
-            cur=cur.next
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        if not l1 or not l2:
+            return l1 or l2
+        head = cur = ListNode(0)
+        while l1 and l2:
+            #print(head)
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        #最後の数字
+        cur.next = l1 or l2
         return head.next
 
-#test=Solution()
+#print(head)
+ListNode{val: 0, next: None}
+ListNode{val: 0, next: ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}
+ListNode{val: 0, next: ListNode{val: 1, next: ListNode{val: 1, next: ListNode{val: 2, next: ListNode{val: 4, next: None}}}}}
+ListNode{val: 0, next: ListNode{val: 1, next: ListNode{val: 1, next: ListNode{val: 2, next: ListNode{val: 4, next: None}}}}}
+ListNode{val: 0, next: ListNode{val: 1, next: ListNode{val: 1, next: ListNode{val: 2, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}}}}
+
+#print(cur)
+ListNode{val: 0, next: None}
+ListNode{val: 1, next: ListNode{val: 3, next: ListNode{val: 4, next: None}}}
+ListNode{val: 1, next: ListNode{val: 2, next: ListNode{val: 4, next: None}}}
+ListNode{val: 2, next: ListNode{val: 4, next: None}}
+ListNode{val: 3, next: ListNode{val: 4, next: None}}
