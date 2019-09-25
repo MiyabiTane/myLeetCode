b0bd15973ce110a4786ca842c39dc85e427c48b8
MiyabiTane <miyabitane@yahoo.co.jp>
2019-09-25 23:02:23 +0900

    wrong when , -42

---
+++ 8_m_String_To_Integer.py
@@ -0,0 +1,37 @@
+def myAtoi(str):
+    list=['0','1','2','3','4','5','6','7','8','9','-']
+    ans=[]
+    flag=0
+    #print(4 in list)
+    #print(str[0] in list)
+    if str=="":
+        return 0
+    for i in range(len(str)):
+        if (str[i] in list)==False and str[i]!=" ":
+            break
+            if flag!=2:
+                return 0
+        elif str[i] in list:
+            flag=2
+            if str[i]=='-':
+                flag=1
+            else:
+                ans.append(str[i])
+    #print(ans)
+    inte=0
+    for i in range(len(ans)):
+        inte+=int(ans.pop(-1))*10**i
+    if flag==1:
+        return -1*inte
+    elif inte<(-2)**31:
+        return (-2)**31
+    elif inte>2**31-1:
+        return 2**31-1
+    else:
+        return inte
+    """ans_map=map(str,ans)
+    if flag==1:
+        result=''-'+'.join(ans_map)
+    else:
+        result=''.join(ans_map)
+    return result"""
