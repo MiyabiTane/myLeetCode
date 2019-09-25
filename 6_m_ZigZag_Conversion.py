e13fef9fd148961386195ec63a322dada5767ccb
MiyabiTane <miyabitane@yahoo.co.jp>
2019-09-25 15:38:28 +0900

    リストの使い方[[],[]]

--- 6_m_ZigZag_Conversion.py
+++ 6_m_ZigZag_Conversion.py
@@ -2,18 +2,21 @@ def convert(s,numRows):
     nums=[[] for i in range(numRows)]
     i=0
     flag=0
-    for word in s:
-        nums[i].append(word)
-        if flag==1 and i!=0:
-            i-=1
-        elif flag==1 and i==0:
-            i+=1
-            flag=0
-        elif i==numRows-1:
-            i-=1
-            flag=1
-        else:
-            i+=1
+    if numRows==1:
+        return s
+    else:
+        for word in s:
+            nums[i].append(word)
+            if flag==1 and i!=0:
+                i-=1
+            elif flag==1 and i==0:
+                i+=1
+                flag=0
+            elif i==numRows-1:
+                i-=1
+                flag=1
+            else:
+                i+=1
         #print(nums)
     str=""
     while nums:
