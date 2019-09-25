48834757a162903c2172acc506bfbe3affb70b64
MiyabiTane <miyabitane@yahoo.co.jp>
2019-09-25 17:33:06 +0900

    リストの使い方[[]]

--- 5_m_Longest_Palindromic.py
+++ 5_m_Longest_Palindromic.py
@@ -1,25 +1,20 @@
 def longestPalindrome(s):
-    list=[0,0]
-    max=0
-    count=0
-    ans=[]
+    max_count=0
+    if s=="":
+        return ""
+    start=0
+    end=0
+    check=[[0 for i in range(len(s))] for j in range(len(s))] #init
     for i in range(len(s)):
-        """if list==[0,0]:
-            if count>max:
-                max=count
-            count=0"""
-        if s[i]==list[-1]:
-            list.pop(i+1)
-            count+=2
-            if count>max:
-                max=count
-        elif s[i]==list[-2]:
-            list.pop(i+1)
-            list.pop(i)
-            count+=3
-            if count>max:
-                max=count
-        else:
-            list.append(s[i])
-
-    return max
+        check[i][i]=1
+        for j in range(0,i):
+            #print(j,i)
+            if s[j]==s[i] and (check[j+1][i-1]==1 or i-j<=2):
+                check[j][i]=1
+                #print("Hello")
+                if i-j>max_count:
+                    max_start=j
+                    max_end=i
+                    max_count=i-j
+    #print(check)
+    return s[max_start:max_end+1]
