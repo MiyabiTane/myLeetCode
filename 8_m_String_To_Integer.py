cd7568a17503e2f0332c57bb0955a4e64c8b6bc0
MiyabiTane <miyabitane@yahoo.co.jp>
2019-09-25 23:35:25 +0900

    wrong when ,- 123

--- 8_m_String_To_Integer.py
+++ 8_m_String_To_Integer.py
@@ -1,32 +1,41 @@
 def myAtoi(str):
-    list=['0','1','2','3','4','5','6','7','8','9','-']
+    list=['0','1','2','3','4','5','6','7','8','9','-','+']
     ans=[]
     flag=0
+    flag2=0
+    flag3=0
     #print(4 in list)
     #print(str[0] in list)
     if str=="":
         return 0
     for i in range(len(str)):
-        if (str[i] in list)==False and str[i]!=" ":
+        if (str[i] in list)==False and str[i]!=' ':
             break
-            if flag!=2:
+            if flag!=1 and flag2!=1:
                 return 0
+        elif flag2==1 and str[i]==' ':
+            break
         elif str[i] in list:
-            flag=2
             if str[i]=='-':
                 flag=1
+            elif str[i]=='+':
+                flag3=1
             else:
                 ans.append(str[i])
+                flag2=1
     #print(ans)
     inte=0
     for i in range(len(ans)):
         inte+=int(ans.pop(-1))*10**i
     if flag==1:
-        return -1*inte
-    elif inte<(-2)**31:
+        inte=-1*inte
+    #print(inte)
+    if inte<(-2)**31:
         return (-2)**31
     elif inte>2**31-1:
         return 2**31-1
+    elif flag==1 and flag3==1:
+        return 0
     else:
         return inte
     """ans_map=map(str,ans)
