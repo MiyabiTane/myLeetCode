b5ea8eb4d1f043d71337cb70d612f2647c289f58
MiyabiTane <miyabitane@yahoo.co.jp>
2019-09-25 20:29:04 +0900

    add file

--- 26_Remove_Duplicates.py
+++ 26_Remove_Duplicates.py
@@ -1,24 +1,10 @@
 def removeDuplicates(nums):
+    leng=0
     if nums==[]:
         return 0
-    mark=nums[0]
-    try:
+    else:
         for i in range(len(nums)):
-            if nums[i]==mark and nums[i]!=nums[-1]:
-                nums.pop(i)
-            mark=nums[i+1]
-            print("%d:%d"%(mark,nums))
-    except:
-        pass
-    print(nums)
-    return len(nums)
-
-    #mark=nums[0]
-    ##because of nums.pop(),out of range occurs
-    #for i in range(len(nums)):
-    #    if nums[i]==mark:
-    #        nums.pop(i)
-    #        if i!=len(nums)-1:
-    #            mark=nums[i]
-    #        print(mark)
-    #return len(nums)
+            if nums[leng]!=nums[i]:
+                leng+=1
+                nums[leng]=nums[i]
+        return leng+1
