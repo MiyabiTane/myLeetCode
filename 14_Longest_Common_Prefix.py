f3d1fe4505135245f1268571e596233edbc90a96
MiyabiTane <miyabitane@yahoo.co.jp>
2019-09-25 17:44:21 +0900

    RunTime Error when [aa,a]

--- 14_Longest_Common_Prefix.py
+++ 14_Longest_Common_Prefix.py
@@ -1,21 +1,10 @@
 def longestCommonPrefix(strs):
-    flag=0
-    flag2=0
-    ans=""
     if strs==[]:
-        ans=""
-    elif len(strs)==1:
-        ans=strs[0]
-    else:
-        while(flag2==0):
-            for i in range(min(len(strs[0]),len(strs[1]))):
-                if strs[0][i]==strs[1][i]:
-                    for j in range(len(strs)):
-                        if strs[j][i]!=strs[0][i]:
-                            flag=1
-                            flag2=1
-                    if flag==0:
-                        ans+=strs[0][i]
-                else:
-                    flag2=1
-    return ans
+        return ""
+    for i in range(len(strs[0])):
+        for string in strs[1:]:
+            if i>=len(string):
+                return strs[0][:i]
+            elif string[i]!=strs[0][i]:
+                return strs[0][:i]
+    return strs[0]
