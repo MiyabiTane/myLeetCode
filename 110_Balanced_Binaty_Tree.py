88b6a211fbda4acfd4c528a531cbe9081fbd31a5
MiyabiTane <miyabitane@yahoo.co.jp>
2019-09-26 09:34:30 +0900

    TreeNode

--- 110_Balanced_Binaty_Tree.py
+++ 110_Balanced_Binaty_Tree.py
@@ -6,36 +6,21 @@ class TreeNode(object):
 
 class Solution(object):
     def isBalanced(self,root):
-        count=0
         if not root:
             return True
-
-        self.iscount(root.left,count,1)
-        self.iscount(root.right,count,-1)
-
-        if -1<=count<=1:
-            return True
-        elif count<-1 or 1<count:
+        elif abs(self.maxcount(root.left)-self.maxcount(root.right))>1:
             return False
-
-    def iscount(self,root,count,num):
-        if root:
-            count+=num
-            return count
-
-"""
-mugen roop
-class Solution(object):
-    def isBalanced(self,root):
-        count=0
-        if not root:
-            return True
-        while root:
-            if root.left:
-                count+=1
-            elif root.right:
-                count-=1
-        if -1<=count<=1:
+        else:
+            return self.isBalanced(root.left) and self.isBalanced(root.right)
+        """
+        #[1,2,2,3,null,null,3,4,null,null,4]->True but actually the answer is False
+        elif abs(self.maxcount(root.left)-self.maxcount(root.right))<=1:
             return True
         else:
             return False"""
+
+    def maxcount(self,root):
+        if not root:
+            return 0
+        else:
+            return max(self.maxcount(root.left),self.maxcount(root.right))+1
