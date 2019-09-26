d72bbd391fc9bab61e19550be628727b9b4e14d4
MiyabiTane <miyabitane@yahoo.co.jp>
2019-09-26 09:04:41 +0900

    動的計画法

--- 53_Maximum_Subarray.py
+++ 53_Maximum_Subarray.py
@@ -1,30 +1,9 @@
-def sum(nums):
-    sum=0
-    for i in nums:
-        sum+=i
-    return sum
-
-def hantei(nums):
-    for i in nums:
-        if i>0:
-            return 0
-    return 1
-
+#dynamic Solution
+#max(now_position,sum_before+now_position)
 def maxSubArray(nums):
-    l=[]
-    max=nums[0]
-    for i in range(len(nums)):
-        sum_before=sum(l)
-        #l.append(nums[i])
-        #if sum(l)<0:
-        #    l=[]
-        if nums[i]<0:
-            max=sum(l)
-        l.append(nums[i])
-
-
-
-        print(l,sum(l))
-    return sum(l)
-    if hantei(nums)==1:
-        return max(nums)
+    max_sum=[]
+    sum_before=-float('inf')
+    for num in nums:
+        max_sum.append(max(num,sum_before+num))
+        sum_before=max_sum[-1]
+    return max(max_sum)
