af5b21627d5af9ee64e625ccaa150d7c3bc37112
MiyabiTane <miyabitane@yahoo.co.jp>
2019-09-26 09:10:00 +0900

    再帰

--- 67_Add_Binary.py
+++ 67_Add_Binary.py
@@ -1,25 +1,13 @@
-def binaryToDecimal(object):
-    dec=0
-    i=0
-    while object!=0:
-        mark=object%10
-        dec+=mark*2**i
-        object=(object-object%10)/10
-        i+=1
-    return dec
-
-def decimalToBinary(object):
-    bin_rev=[]
-    while object!=0:
-        bin_rev.append(object%2)
-        object=(object-object%2)/2
-        #print(bin_rev)
-    bin_rev.reverse()
-    num=0
-    for i in range(len(bin_rev)):
-        num+=bin_rev.pop()*10**i
-    return num
-
 def addBinary(a,b):
-    sum_dec=binaryToDecimal(a)+binaryToDecimal(b)
-    return decimalToBinary(sum_dec)
+    if (not a):
+        return b
+    elif (not b):
+        return a
+    else:
+        if a[-1]=='0' and b[-1]=='0':
+            return addBinary(a[:-1],b[:-1])+'0'
+        elif a[-1]=='1' and b[-1]=='1':
+            return addBinary(addBinary(a[:-1],b[:-1]),'1')+'0'
+        else:
+            return addBinary(a[:-1],b[:-1])+'1'
+            
