f3f3c0f06876335e112c883d87d8b2fd78cb7707
MiyabiTane <miyabitane@yahoo.co.jp>
2019-09-26 09:22:25 +0900

    ListNode nextの使い方

--- 83_Remove_Duplicates.py
+++ 83_Remove_Duplicates.py
@@ -1,22 +1,11 @@
-class ListNode(object):
-    def __init__(self,x):
-        self.val=x
-        self.next=None
-
 class Solution(object):
     def deleteDuplicates(self,head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
-        cur=ans=ListNode(0)
-
-        while head:
-                forward=head.val
-                ans.next=forward
-                head=head.next
-                if forward!=head.val:
-                    ans=ans.next
-                    ans.next=head.val
-                ans=ans.next
-        return cur.next
+        cur=head
+        while cur:
+            if cur.next and cur.next.val==cur.val:
+                cur.next=cur.next.next
+            else:
+                cur=cur.next
+            #print(cur)
+            #print(head)
+        return head
