a12af2ce428c2596de14c3b01f76f2625aae6c25
MiyabiTane <miyabitane@yahoo.co.jp>
2019-09-26 15:42:47 +0900

    TreeNode

--- 112_Path_Sum.py
+++ 112_Path_Sum.py
@@ -8,8 +8,9 @@ class Solution(object):
     def hasPathSum(self,root,sum):
         if not root:
             return False
+        elif
         else:
-            if root.val==sum:
+            if root.val==sum and ((not root.left) and (not root.right)):
                 return True
             else:
                 return self.hasPathSum(root.left,sum-root.val) or self.hasPathSum(root.right,sum-root.val)
