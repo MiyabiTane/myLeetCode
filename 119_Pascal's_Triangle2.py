7222c9e5aec518186246f8d3a4adc8e1a732ce94
MiyabiTane <miyabitane@yahoo.co.jp>
2019-09-28 15:14:47 +0900

    o(k)

--- 119_Pascal's_Triangle2.py
+++ 119_Pascal's_Triangle2.py
@@ -1,5 +1,15 @@
 def getRow(rowIndex):
-    rowIndex+=1
+    #init
+    ans=[1 for j in range(rowIndex+1)]
+    #c(i,j)=i!/(j!*(n-j)!)
+    #c(i-1,j)=c(i,j)*(i-j)/i
+    if rowIndex>1:
+        for i in range(1,rowIndex//2+1):
+            ans[i]=ans[rowIndex-i]=ans[i-1]*(rowIndex-i+1)/i
+    return ans
+
+
+    """rowIndex+=1
     if rowIndex==0:
         return []
     #init
@@ -8,4 +18,4 @@ def getRow(rowIndex):
         if i>1:
             for j in range(1,i):
                 ans[i][j]=ans[i-1][j-1]+ans[i-1][j]
-    return ans[-1]
+    return ans[-1]"""
