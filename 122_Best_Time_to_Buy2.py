8a9fe968e97ff82a36eba53ca2ae00b2835b96e7
MiyabiTane <miyabitane@yahoo.co.jp>
2019-10-02 22:08:46 +0900

    more simple but later solution

--- 122_Best_Time_to_Buy2.py
+++ 122_Best_Time_to_Buy2.py
@@ -1,6 +1,13 @@
-#when there is vally and peak, we alwaly increase the profit
 def maxProfit(prices):
     if len(prices)<2:
+        return 0
+    pro=0
+    for i in range(len(prices)-1):
+        if prices[i+1]>prices[i]:
+            pro+=prices[i+1]-prices[i]
+    return pro
+
+    """if len(prices)<2:
         return 0
     valley=prices[0]
     peak=0
@@ -17,4 +24,4 @@ def maxProfit(prices):
             flag=1
     if flag==1:
         pro+=peak-valley
-    return pro
+    return pro"""
