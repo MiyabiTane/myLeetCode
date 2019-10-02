359d20cebe0d6287aee523d8a50990400708d848
MiyabiTane <miyabitane@yahoo.co.jp>
2019-10-02 21:34:30 +0900

    wrong when [1,4,2]

--- 122_Best_Time_to_Buy2.py
+++ 122_Best_Time_to_Buy2.py
@@ -2,20 +2,26 @@
 def maxProfit(prices):
     if len(prices)<2:
         return 0
-    valley=0
+    valley=-1
     peak=0
     pro=0
     flag=0
     for i in range(1,len(prices)):
         if prices[i]<prices[i-1]:
-            if flag==2:
+            if flag==2 and valley!=-1:
                 pro+=peak-valley
             valley=prices[i]
             flag=1
-        elif flag==1 and prices[i]>prices[i-1]:
+        elif prices[i]>prices[i-1]:
             peak=prices[i]
             flag=2
-        #print(flag)
-    if flag==0:
-        pro=prices[-1]-prices[0]
+        print(prices[i],"flag,pro= ",flag,pro)
+    print("")
+    """if flag==0:
+        pro=prices[-1]-prices[0]"""
+    if flag==2:
+        if valley==-1:
+            pro=prices[-1]-prices[0]
+        else:
+            pro+=peak-valley
     return pro
