6919eafce1b00a6ea9245249674fbec4b6d2c61b
MiyabiTane <miyabitane@yahoo.co.jp>
2019-10-02 21:52:39 +0900

    add file

--- 122_Best_Time_to_Buy2.py
+++ 122_Best_Time_to_Buy2.py
@@ -2,26 +2,19 @@
 def maxProfit(prices):
     if len(prices)<2:
         return 0
-    valley=-1
+    valley=prices[0]
     peak=0
     pro=0
     flag=0
-    for i in range(1,len(prices)):
-        if prices[i]<prices[i-1]:
-            if flag==2 and valley!=-1:
+    for i in range(len(prices)-1):
+        if prices[i+1]<prices[i]:
+            if flag==1:
                 pro+=peak-valley
-            valley=prices[i]
+            valley=prices[i+1]
+            flag=0
+        elif prices[i+1]>prices[i]:
+            peak=prices[i+1]
             flag=1
-        elif prices[i]>prices[i-1]:
-            peak=prices[i]
-            flag=2
-        print(prices[i],"flag,pro= ",flag,pro)
-    print("")
-    """if flag==0:
-        pro=prices[-1]-prices[0]"""
-    if flag==2:
-        if valley==-1:
-            pro=prices[-1]-prices[0]
-        else:
-            pro+=peak-valley
+    if flag==1:
+        pro+=peak-valley
     return pro
