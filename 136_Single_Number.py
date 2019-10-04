4929ef5447d3ff0f883449b1015e7dad17b9747d
MiyabiTane <miyabitane@yahoo.co.jp>
2019-10-04 21:28:58 +0900

    辞書 keyの取り出し方

--- 136_Single_Number.py
+++ 136_Single_Number.py
@@ -8,10 +8,4 @@ def singleNumber(nums):
         #print(dict)
     return dict.popitem()[0] #pop the key, popitem() pop key and value
 
-    """al_ex_num=[]
-    for num in nums:
-        if num in al_ex_num:
-            al_ex_num.pop(al_ex_num.index(num))
-        else:
-            al_ex_num.append(num)
-    return al_ex_num.pop(0)"""
+    
