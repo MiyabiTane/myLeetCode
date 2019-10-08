c99519cd5fba7dffa95cfade38e11cce329e4f76
MiyabiTane <miyabitane@yahoo.co.jp>
2019-10-08 19:38:07 +0900

    Time Limit Exceeded

--- 167_Two_Sum_2.py
+++ 167_Two_Sum_2.py
@@ -1,14 +1,5 @@
 def TwoSum(numbers,target):
-    ans=[]
     for i in range(len(numbers)):
-        num_want=target-numbers[i]
-        if num_want in numbers and num_want!=numbers[i]:
-            ans.append(i+1)
-            ans.append(numbers.index(num_want)+1)
-            break
-        elif num_want in numbers and num_want==numbers[i]:
-            if i!=len(numbers)-1:
-                if num_want==numbers[i+1]:
-                    ans.append(i+1)
-                    ans.append(numbers.index(num_want)+2)
-    return ans
+        for j in range(i+1,len(numbers)):
+            if numbers[i]+numbers[j]==target:
+                return [i+1,j+1]
