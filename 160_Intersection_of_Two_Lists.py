522573e396d4868e2594b2a32514489c4a0fff55
MiyabiTane <miyabitane@yahoo.co.jp>
2019-10-13 00:26:25 +0900

    Two Pointer ListNode

--- 160_Intersection_of_Two_Lists.py
+++ 160_Intersection_of_Two_Lists.py
@@ -5,26 +5,26 @@ class ListNode(object):
 
 class Solution(object):
     def getIntersectionNode(self,headA,headB):
-        countA=0
-        countB=0
-        curA=headA
-        curB=headB
-        while curA:
-            countA+=1
-            cueA=curA.next
-        while curB:
-            countB+=1
-            curB=curB.next
-        while countA>countB:
-            headA=headA.next
-            countA-=1
-        while countB>countA:
-            headB=headB.next
-            countB-=1
-        while curA and curB:
-            if curA==curB:
-                return curA
+        pA=headA
+        pB=headB
+        count=0
+        while True:
+            if count==2:
+                break
+            elif pA==pB:
+                return pA
+            elif pA or pB:
+                if pA==None:
+                    pA=headB
+                    pB=pB.next
+                    count+=1
+                elif pB==None:
+                    pA=pA.next
+                    pB=headA
+                else:
+                    pA=pA.next
+                    pB=pB.next
             else:
-                curA=curA.next
-                curB=curB.next
+                pA=headB
+                pB=headA
         return None
