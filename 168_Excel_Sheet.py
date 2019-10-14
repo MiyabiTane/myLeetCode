293dd3bb7c94acbdadef4ea417e7ec751e68d021
MiyabiTane <miyabitane@yahoo.co.jp>
2019-10-14 16:21:36 +0900

    add file

---
+++ 168_Excel_Sheet.py
@@ -0,0 +1,11 @@
+def convertToTitle(n):
+    dict={1:'A',2:'B',3:'C',4:'D',5:'E',6:'F',7:'G',8:'H',9:'I',10:'J',11:'K',12:'L',13:'M',14:'N',15:'O',16:'P',17:'Q',18:'R',19:'S',20:'T',21:'U',22:'V',23:'W',24:'X',25:'Y',26:'Z'}
+    if n<=26:
+        return dict.get(n)
+    else:
+        div=(n-n%26)/26
+        rem=(n%26)
+        if rem==0:
+            rem=26
+            div-=1
+        return convertToTitle(div)+convertToTitle(rem)
