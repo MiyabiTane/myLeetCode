f4f025fc1b4431fbe255401652fbfe8eed48f5a8
MiyabiTane <miyabitane@yahoo.co.jp>
2019-10-20 16:59:18 +0900

    add file

--- 172_Factorial_Trailing_Zeros.py
+++ 172_Factorial_Trailing_Zeros.py
@@ -1,11 +1,10 @@
-import math
-
 def trailingZeros(n):
-    n_fac=math.factorial(n)
     ans=0
-    amari=n_fac%10
-    while amari==0:
-        ans+=1
-        n_fac/=10
-        amari=n_fac%10
+    count=1
+    while True:
+        if n>=5**count:
+            ans+=(n//(5**count))
+            count+=1
+        else:
+            break
     return ans
