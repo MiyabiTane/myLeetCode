19774635b233c84c3623bafd7125ab0c44651f82
MiyabiTane <miyabitane@yahoo.co.jp>
2019-10-25 22:59:44 +0900

    wrong when len(nums)<k

---
+++ 189_Rotate_Array.py
@@ -0,0 +1,16 @@
+def rotate(nums,k):
+    if len(nums)<2:
+        return nums
+    nums_v=[]
+    for i in range(len(nums)):
+        nums_v.append(nums[i])
+    #nums_v=nums <-nums_v changes every time when i change
+    #print(nums_v)
+    #print('')
+    for i in range(len(nums_v)):
+        nums[i]=nums_v[i-k]
+        #print("nums = {}, nums_v = {}".format(nums,nums_v))
+        #print("nums_v[{}] is {} ".format(i-k,nums_v[i-k]))
+    return nums
+    #nums=nums[-k:]+nums[:-k] <= not in-place
+    
