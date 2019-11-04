2c2b3aae3e4d37e4a5adf870c05207f60d582b49
MiyabiTane <miyabitane@yahoo.co.jp>
2019-11-04 21:18:36 +0900

    辞書 keyで降順ソート

--- 12_m_Integer_to_Roman.py
+++ 12_m_Integer_to_Roman.py
@@ -1,30 +1,12 @@
 def intToRoman(num):
-    #dict={1:'I',5:'V',10:'X',50:'L',100:'C',500:'D',1000:'M'}
-    if num==0:
-        return ''
-    elif num<=3:
-        return "I"+intToRoman(num-1)
-    elif num==4:
-        return "IV"+intToRoman(num-4)
-    elif 5<=num and num<9:
-        return "V"+intToRoman(num-5)
-    elif num==9:
-        return "IX"+intToRoman(num-9)
-    elif 10<=num and num<40:
-        return "X"+intToRoman(num-10)
-    elif 40<=num and num<50:
-        return "XL"+intToRoman(num-40)
-    elif 50<=num and num<90:
-        return "L"+intToRoman(num-50)
-    elif 90<=num and num<100:
-        return "XC"+intToRoman(num-90)
-    elif 100<=num and num<400:
-        return "C"+intToRoman(num-100)
-    elif 400<=num and num<500:
-        return "CD"+intToRoman(num-400)
-    elif 500<=num and num<900:
-        return "D"+intToRoman(num-500)
-    elif 900<=num and num<1000:
-        return "CM"+intToRoman(num-900)
-    else:
-        return "M"+intToRoman(num-1000)
+    dict={1000:'M',900:'CM',500:'D',400:'CD',100:'C',90:'XC',50:'L',40:'XL',10:'X',9:'IX',5:'V',4:'IV',1:'I'}
+    ans=""
+    #dict doesn't have order
+    keys=sorted(dict,reverse=True)
+    for key in keys:
+        #print(key)
+        while key<=num:
+            #print("get")
+            ans+=dict.get(key)
+            num-=key
+    return ans
