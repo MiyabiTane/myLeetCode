dcd5c4a4593b96094a041a13dc3747fd07cf7ac6
MiyabiTane <miyabitane@yahoo.co.jp>
2019-11-25 01:29:57 +0900

    Wrong when [-1,0,1,0]

--- 15_m_3Sum.py
+++ 15_m_3Sum.py
@@ -1,19 +1,19 @@
 def threeSum(nums):
+    nums.sort()
     ans=[]
     if len(nums)<3:
-        return []
-    for i in range(len(nums)-2):
-        for j in range(i+1,len(nums)-1):
-            sum=nums[i]+nums[j]
-            if -1*sum in nums[j+1:]:
-                ans.append([nums[i],nums[j],-sum])
-    #print(ans)
-    l=len(ans[0])-1
-    while l>0:
-        #print(l)
-        #print([ans[l][0],ans[l][1],ans[l][2]])
-        if ([ans[l][0],ans[l][1],ans[l][2]] in ans[:l]) or ([ans[l][0],ans[l][2],ans[l][1]] in ans[:l]) or ([ans[l][1],ans[l][0],ans[l][2]] in ans[:l]) or ([ans[l][1],ans[l][2],ans[l][0]] in ans[:l]) or ([ans[l][2],ans[l][0],ans[l][1]] in ans[:l]) or ([ans[l][2],ans[l][1],ans[l][0]] in ans[:l]):
-            ans.pop(l)
-            l-=1
-        l-=1
+        return ans
+    for i in range(len(nums)):
+        if i!=0 and nums[i]==nums[i-1]:
+            #print("continue")
+            continue
+        target=-nums[i]
+        check=[]
+        for j in range(i+1,len(nums)):
+            if j>1 and nums[j]==nums[j-2]:
+                continue
+            target_2=target-nums[j]
+            if (target_2 in nums[j+1:])and (not (target_2 in check)):
+                ans.append([nums[i],nums[j],target_2])
+                check.append(nums[j]
     return ans
