a68174f17f3f613f98bdb4725e48f930bb7cd452
MiyabiTane <miyabitane@yahoo.co.jp>
2020-02-24 18:52:25 +0900

    ListNode 新しいリストノードを作成

--- 2_m_Add_Two_Numbers.py
+++ 2_m_Add_Two_Numbers.py
@@ -5,46 +5,24 @@ class ListNode(object):
 
 class Solution(object):
     def addTwoNumbers(self,l1,l2):
-        #リスト→数字
-        def GetNum(lst):
-            num_list=[]
-            while lst is not None:
-                num_list.append(lst.val)
-                lst=lst.next
-            return num_list
-
-        #２つの数字の和→リスト(左が１の位)
-        def GetSum(num_l1,num_l2):
-            sum1=0; sum2=0
-            for i in range(len(num_l1)):
-                sum1+=num_l1.pop()*(10**i)
-            for i in range(len(num_l2)):
-                sum2+=num_l2.pop()*(10**i)
-            sum=sum1+sum2
-            get_sum=[]
-            while True:
-                get_sum.append(sum%10)
-                if sum<10:
-                    break
-                sum=sum//10
-            return get_sum
-
-        #リスト→リストノード
-        def NumtoList(sum_list):
-            answer=ListNode(0)
-            dummy=answer
-            for i in range(len(sum_list)):
-                dummy.next=sum_list[i]
-                dummy=dummy.next #種悪の根源。ここでdummyがintに
-            return answer.next
-
-        num_l1=GetNum(l1)
-        num_l2=GetNum(l2)
-        sum_list=GetSum(num_l1,num_l2)
-        answer=NumtoList(sum_list)
-        return answer
-
-
-
-
-
+       answer=dummy=ListNode(0)
+        carry=0   #繰り上がりの記憶
+        while l1 or l2 or carry==1:
+            num1=l1.val if l1 else 0
+            num2=l2.val if l2 else 0
+            #print(num1,num2)
+            sum=num1+num2+carry
+            if sum<10:
+                carry=0
+            else:
+                carry=1 
+            dummy.next=ListNode(sum%10) 
+            #print(answer)
+            if l1:
+                l1=l1.next
+            if l2:
+                l2=l2.next
+            #print(l1)
+            #print(l2)
+            dummy=dummy.next
+        return answer.next
