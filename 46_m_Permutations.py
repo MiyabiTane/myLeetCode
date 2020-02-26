da1950b13ff0ea229418a428f8f6f1c83b756a8c
MiyabiTane <miyabitane@yahoo.co.jp>
2020-02-26 20:41:57 +0900

    順列アルゴリズム バックトラックに似てる

--- 46_m_Permutations.py
+++ 46_m_Permutations.py
@@ -1,31 +1,29 @@
 def permute(nums):
-    def sub_permute(inte,nums):
-        if len(nums)==2:
-            inte2=inte.cppy()
-            inte.append(nums[0])
-            inte.append(nums[1])
-            inte2.append(nums[1])
-            inte2.append(nums[0])
-            return [inte,inte2]
-        else:
-            list=[]
-            for i in range(len(nums)):
-                copy=nums.copy()
-                copy.pop(i)
-                list.append(sub_permute(nums[i],copy))
-            return list
-    
-    if len(nums)==0:
-        return []
-    elif len(nums)==1:
-        return [nums]
-    elif len(nums)==2:
-        return [[nums[0],nums[1]],[nums[1],nums[0]]]
-    else:
+    #count:[1,2,3]->[1,1,1]
+    #tmp:順列ひとつひとつ
+    #d:一つの順列を作り終えたか判断する
+    def like_backtrack(nums,count,tmp,ans,d):
+        if d==len(nums):
+            copy=tmp.copy()
+            ans.append(copy)
+            return #ひとつ前のステップに戻る
+        for i in range(len(nums)):
+            if count[i]==0:
+                continue
+            count[i]-=1
+            tmp[d]=nums[i]
+            like_backtrack(nums,count,tmp,ans,d+1)
+            count[i]+=1
+            tmp[d]=0
 
+    count=[1]*len(nums)
+    ans=[]
+    tmp=[0]*len(nums)
+    like_backtrack(nums,count,tmp,ans,0)
+    return ans
 
 ans=permute([1,2,3])
 print(ans)
 
 
-    
+#解説動画:https://www.youtube.com/watch?time_continue=3&v=nYFd7VHKyWQ&feature=emb_title
