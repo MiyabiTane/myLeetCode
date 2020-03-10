b5e5d8e643e579dee090a502071c5460fcce6b47
MiyabiTane <miyabitane@yahoo.co.jp>
2020-03-10 22:48:03 +0900

    DP トップダウン(Time Limit Exceeded)

--- 55_m_Jump_Game.py
+++ 55_m_Jump_Game.py
@@ -1,14 +1,23 @@
+#トップダウンDP : 一度計算したものは記憶しておいて２回目は計算しないようにする。
 def canJump(nums):
-    #i:後ろからのステップ数
-    for i in range(len(nums)):
-        num=nums[len(nums)-1-i]
-        if num==i and i==len(nums)-1:
+    #そこから初めて最後のマスにたどり着ける -> Good
+    #たどり着けない -> Bad
+    memo =["N"]*len(nums) #Good or False
+    def canJumpFromPosition(pos, nums):
+        if memo[pos]=="G": 
             return True
-        elif num==i:
-            return canJump(nums[:-i])
-        elif i==len(nums)-1:
-            return False
+        furtherstJump=min(pos+nums[pos], len(nums)-1)
+        for i in range(pos+1, furtherstJump+1):
+            if canJumpFromPosition(i, nums):
+                memo[pos]="G"
+                return True
+        memo[pos]="B"
+        return False
 
-ans=canJump([2,3,1,1,4])
+    memo[-1]="G"
+    return canJumpFromPosition(0, nums)
+
+
+ans=canJump([3,2,1,0,4])
 print(ans)
 
