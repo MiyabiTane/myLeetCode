d44306f3328b764b29a17260838bbe303b37dd6a
MiyabiTane <miyabitane@yahoo.co.jp>
2020-04-05 22:21:25 +0900

    in-place（入力リスト編集しながら）

--- 30-Day_Challenge/4_Move_Zeros.py
+++ 30-Day_Challenge/4_Move_Zeros.py
@@ -1,13 +1,16 @@
 def moveZeros(nums):
-    pos=0
-    for i in range(len(nums)):
-        if nums[pos]==0:
-            nums = nums[:pos]+nums[pos+1:]+[nums[pos]]
-        else:
-            pos+=1
+    p1=0; p2=0
+    while p2!=len(nums):
+        while nums[p2]==0:
+            p2+=1
+            if p2==len(nums):
+                return nums
+        if p1!=p2:
+            nums[p1],nums[p2] = nums[p2],nums[p1]
+        p1+=1; p2+=1
     return nums
 
-ans = moveZeros([0, 1, 0, 3, 12])
+ans = moveZeros([0])
 print(ans)
 
 
