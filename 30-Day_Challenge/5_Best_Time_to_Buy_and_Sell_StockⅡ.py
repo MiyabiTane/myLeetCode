a9e8a8ea64b3813b3407c04222f34409778677e8
MiyabiTane <miyabitane@yahoo.co.jp>
2020-04-05 22:58:56 +0900

    Simple One Pass

--- 30-Day_Challenge/5_Best_Time_to_Buy_and_Sell_StockⅡ.py
+++ 30-Day_Challenge/5_Best_Time_to_Buy_and_Sell_StockⅡ.py
@@ -1,5 +1,19 @@
 def maxProfit(prices):
     profit = 0
+    for i in range(len(prices)-1):
+        if prices[i]<prices[i+1]:
+            profit+=(prices[i+1]-prices[i])
+    return profit
+
+
+ans = maxProfit([7, 6, 4, 3, 1])
+print(ans)
+
+"""
+def maxProfit(prices):
+    if len(prices)<2:
+        return 0
+    profit = 0
     pos = 0
     while pos<=len(prices)-1:
         while prices[pos] > prices[pos+1]:
@@ -13,9 +27,8 @@ def maxProfit(prices):
                 return profit+prices[pos]
         profit += prices[pos]
     return profit
+"""
 
 
-ans = maxProfit([7, 1, 5, 3, 6, 4])
-print(ans)
 
 
