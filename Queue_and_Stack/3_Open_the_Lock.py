1d4a8eca2d1522884f13f97b588a0bbb9b6c9bb8
MiyabiTane <miyabitane@yahoo.co.jp>
2020-04-10 22:28:01 +0900

    Time Limit Exceeded

--- Queue_and_Stack/3_Open_the_Lock.py
+++ Queue_and_Stack/3_Open_the_Lock.py
@@ -1,26 +1,27 @@
 from collections import deque
 def openLock(deadends, target):
-    qu = deque([(0,0,0,0)])
-    count = [0]*4
+    qu = deque([("0000",0)])
+    visited = ["0000"]
     while qu:
-        i,j,k,l = qu.popleft()
-        if str(i)+str(j)+str(k)+str(l)==target:
-            return min(count)
-        if not str(i)+str(j)+str(k)+str((l+1)%10) in deadends:
-            qu.extend([(i,j,k,(l+1)%10)])
-            count[0]+=1
-        if not str(i)+str(j)+str((k+1)%10)+str(l) in deadends:
-            qu.extend([(i,j,(k+1)%10,l)])
-            count[1]+=1
-        if not str(i)+str((j+1)%10)+str(k)+str(l) in deadends:
-            qu.extend([(i,(j+1)%10,k,l)])
-            count[2]+=1
-        if not str((i+1)%10)+str(j)+str(k)+str(l) in deadends:
-            qu.extend([((i+1)%10,j,k,l)])
-            count[3]+=1
-        print(count)
-        if len(qu)==0:
-            return -1
+        num, count = qu.popleft()
+        if num == target:
+            return count
+        elif num in deadends:
+            continue
+        for i in range(4):
+            #数字を一つ上げる
+            new_num = num[:i]+str((int(num[i])+1)%10)+num[i+1:]
+            if not new_num in visited:
+                visited.append(new_num)
+                qu.extend([(new_num,count+1)])
+            #数字を一つ下げる
+            new_num = num[:i]+str((int(num[i])+9)%10)+num[i+1:]
+            if not new_num in visited:
+                visited.append(new_num)
+                qu.extend([(new_num, count+1)])
+    return -1
+
+
 
 
 ans = openLock(["0201", "0101", "0102", "1212", "2002"], "0202")
