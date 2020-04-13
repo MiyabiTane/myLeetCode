604923e896741843e73d21b8c566b06066cfed05
MiyabiTane <miyabitane@yahoo.co.jp>
2020-04-13 19:49:07 +0900

    TLE

--- 30-Day_Challenge/13_Contiguous_Array.py
+++ 30-Day_Challenge/13_Contiguous_Array.py
@@ -1,12 +1,19 @@
 def findMaxLength(nums):
-    if len(nums)==0:
-        return 0
-    
-    num_zero = nums.count(0)
-    num_one = nums.count(1)
-    if num_zero == num_one:
-        return len(nums)
-    return max(findMaxLength(nums[1:]), findMaxLength(nums[:-1]))
+    memo = [0]*(len(nums)+1)
+    max_len = 0
+    #0がきたら-1,1がきたら+1
+    for i,num in enumerate(nums):
+        i+=1
+        if num == 0:
+            memo[i] = memo[i-1]-1
+        if num == 1:
+            memo[i] = memo[i-1]+1
+        if memo[i] in memo:
+            if (i - memo.index(memo[i])) > max_len: #重複したら一番最初のindexを返す
+                max_len = i - memo.index(memo[i])
+        print(memo)
+    return max_len
 
-ans = findMaxLength([1,4,1,1,5,3])
+
+ans = findMaxLength([0,1])
 print(ans)
