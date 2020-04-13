c73364ab3aaa1292a967818d35a2d9de4fd64a85
MiyabiTane <miyabitane@yahoo.co.jp>
2020-04-13 19:54:13 +0900

    ちょっとした工夫でTLE回避できる

--- 30-Day_Challenge/13_Contiguous_Array.py
+++ 30-Day_Challenge/13_Contiguous_Array.py
@@ -1,19 +1,19 @@
 def findMaxLength(nums):
-    memo = [0]*(len(nums)+1)
+    memo = {0:-1}
     max_len = 0
+    sum = 0
     #0がきたら-1,1がきたら+1
     for i,num in enumerate(nums):
-        i+=1
         if num == 0:
-            memo[i] = memo[i-1]-1
+            sum -= 1
         if num == 1:
-            memo[i] = memo[i-1]+1
-        if memo[i] in memo:
-            if (i - memo.index(memo[i])) > max_len: #重複したら一番最初のindexを返す
-                max_len = i - memo.index(memo[i])
-        print(memo)
+            sum += 1
+        if sum in memo:
+            max_len = max(max_len, i-memo[sum])
+        else:
+            memo[sum] = i
     return max_len
 
 
-ans = findMaxLength([0,1])
+ans = findMaxLength([0,1,0])
 print(ans)
