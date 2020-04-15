a5cce68bdba13412946ac90b76cf86a316af3271
MiyabiTane <miyabitane@yahoo.co.jp>
2020-04-15 23:22:29 +0900

    Left and Right List (AC)

--- 30-Day_Challenge/15_Product_of_Array_Except_Self.py
+++ 30-Day_Challenge/15_Product_of_Array_Except_Self.py
@@ -1,15 +1,17 @@
 def productExceptSelf(nums):
-    def make_products(nums, index):
-        pro = 1
-        for i,num in enumerate(nums):
-            if i != index:
-                pro *= num
-        return pro
-    out = []
+    left_product = [1]*len(nums)
+    right_product = [1]*len(nums)
+    for i in range(1,len(nums)):
+        pro = left_product[i-1]*nums[i-1]
+        left_product[i] = pro
+    #print(left_product)
+    for i in range(len(nums)-2,-1,-1):
+        pro = right_product[i+1]*nums[i+1]
+        right_product[i] = pro
+    #print(right_product)
     for i in range(len(nums)):
-        out.append(make_products(nums, i))
-    return out
-
+        left_product[i] *= right_product[i]
+    return left_product
 
 ans = productExceptSelf([1, 2, 3, 4])
 print(ans)
