1da10732cebba05d51a72669503963b7f2ed5eca
MiyabiTane <miyabitane@yahoo.co.jp>
2020-04-21 23:57:58 +0900

    Pointer from RightTop to LeftBottom

--- 30-Day_Challenge/21_Leftmost_Column_with_at_Least_a_One.py
+++ 30-Day_Challenge/21_Leftmost_Column_with_at_Least_a_One.py
@@ -7,64 +7,35 @@
 #    def dimensions(self) -> list[]:
 
 
-def BinarySearch(row):
-    m = len(row)
-    left = 0
-    right = m - 1
-    mid = (left + right) // 2
-    #print(mid)
-    while left < right:
-        mid_num = row[mid]
-        if mid != 0:
-            mid_minus_1 = row[mid-1]
-            if mid_num == 1 and mid_minus_1 == 0:
-                return mid
-        if mid != m-1:
-            mid_plus_1 = row[mid+1]
-            if mid_num == 0 and mid_plus_1 == 1:
-                return mid+1
-        if mid_num == 0:
-            left = mid + 1
-        if mid_num == 1:
-            right = mid - 1
-    return -1
+# """
+# This is BinaryMatrix's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class BinaryMatrix(object):
+#    def get(self, x: int, y: int) -> int:
+#    def dimensions(self) -> list[]:
 
-ans = BinarySearch([0,1])
-print(ans)
-
-"""
 class Solution:
     def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
-        n = binaryMatrix.dimensions(binaryMatrix)[0]
-        #最も左にある1のインデックスを返す
-        def BinarySearch(row_num, BinaryMatrix):
-            m = binaryMatrix.dimensions(binaryMatrix)[1]
-            left = 0; right = m - 1
-            mid = (left + right) // 2
-            while left <= right:
-                mid_num = binaryMatrix.get(row_num, mid)
-                if mid_num == 1 and mid == 0:
-                    return 0
-                mid_minus_1 = binaryMatrix.get(row_num, mid-1)
-                if mid_num == 1 and mid_minus_1 == 0:
-                    return mid
-                mid_plus_1 = binaryMatrix.get(row_num, mid+1)
-                if mid_num == 0 and mid_plus_1 == 1:
-                    return mid+1
-                elif mid_num == 0:
-                    left = mid + 1
-                elif mid_num == 1:
-                    right = mid - 1
+        if not binaryMatrix:
             return -1
-
-        ans = 1000000000
-        for i in range(n):
-            keep = BinarySearch(i, binaryMatrix)
-            if keep < ans:
-                ans = keep
-        return ans
-"""           
+        m = binaryMatrix.dimensions()[0]
+        n = binaryMatrix.dimensions()[1]
+        pointer = [m-1, n-1]
+        while True:
+            num = binaryMatrix.get(pointer[0], pointer[1])
+            if num == 0:
+                if pointer[0] == 0:
+                    if pointer[1] == n-1:
+                        return -1
+                    return pointer[1]+1
+                pointer[0] -= 1
+            else:
+                if pointer[1] == 0:
+                    return 0
+                pointer[1] -= 1
+        
         
 
-                
+
 
