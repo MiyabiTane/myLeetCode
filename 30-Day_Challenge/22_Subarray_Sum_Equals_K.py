a017c59ef354236521d797ac254ad0ca8c87a6f1
MiyabiTane <miyabitane@yahoo.co.jp>
2020-04-23 00:25:29 +0900

    ハッシュテーブル

--- 30-Day_Challenge/22_Subarray_Sum_Equals_K.py
+++ 30-Day_Challenge/22_Subarray_Sum_Equals_K.py
@@ -1,15 +1,21 @@
 def subarraySum(nums,k):
+    sum = 0
+    map = {0:1}
     count = 0
-    sum = [0]
-    for i in range(1,len(nums)+1):
-        sum.append(sum[i-1]+nums[i-1])
-    for i in range(0,len(nums)):
-        for j in range(i+1,len(nums)+1):
-            if sum[j] - sum[i] == k:
-                count+=1
+    for num in nums:
+        sum += num
+        if (sum - k) in map:
+        # sum - k in map ということは 和がsum - kになった場所の次のインデックスからの和がkということ
+            count += map.get(sum-k)
+        if sum in map:
+            map[sum] += 1
+        else:
+            map[sum] = 1
+        
+    #print(map)
     return count
 
-ans = subarraySum([1,1,1],2)
+ans = subarraySum([1,2,1,2,1],3)
 print(ans)
 
 
