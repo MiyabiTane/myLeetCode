c5079c3894837b42d65a8b96c3fe6319ca237a52
MiyabiTane <miyabitane@yahoo.co.jp>
2020-04-26 18:53:10 +0900

    DP

--- 30-Day_Challenge/26_Longest_Common_Subsequence.py
+++ 30-Day_Challenge/26_Longest_Common_Subsequence.py
@@ -6,23 +6,11 @@ def longestCommonSubsequence(text1, text2):
               max(DP[i][j-1], DP[i-1][j]) (else)
     """
     m = len(text1); n = len(text2)
-    DP = [[0 for _ in range(n)] for _ in range(m)]
-    if text1[0] == text2[0]:
-        DP[0][0] = 1
-    #1行目更新
-    for j in range(1,n):
-        DP[0][j] = DP[0][j-1]
-        if text1[0]== text2[j]:
-            DP[0][j] += 1   
-    #1列目更新
-    for i in range(1,m):
-        DP[i][0] = DP[i-1][0]
-        if text1[i] == text2[0]:
-            DP[i][0] += 1
-    #残りの更新
-    for i in range(1,m):
-        for j in range(1,n):
-            if text1[i] == text2[j]:
+    DP = [[0 for _ in range(n+1)] for _ in range(m+1)]
+    
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if text1[i-1] == text2[j-1]:
                 DP[i][j] = DP[i-1][j-1] + 1
             else:
                 DP[i][j] = max(DP[i-1][j], DP[i][j-1])
