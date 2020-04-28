118b350f69e15640dfa6eadfffa71f9e1a11a43b
MiyabiTane <miyabitane@yahoo.co.jp>
2020-04-28 22:18:17 +0900

    TLE

--- 30-Day_Challenge/28_First_Unique_Number.py
+++ 30-Day_Challenge/28_First_Unique_Number.py
@@ -1,6 +1,4 @@
-import collections
 class FirstUnique:
-
     def __init__(self, nums):
         self.queue = nums
         self.unique = []
@@ -10,7 +8,7 @@ class FirstUnique:
                 self.unique.pop(self.unique.index(num))
                 if not num in self.dupli:
                     self.dupli.append(num)
-            else:
+            elif not num in self.dupli:
                 self.unique.append(num)
 
     def showFirstUnique(self):
@@ -21,16 +19,18 @@ class FirstUnique:
         return -1
 
     def add(self, value):
+        #print("add",value)
         if value in self.unique:
             self.unique.pop(self.unique.index(value))
             if not value in self.dupli:
                 self.dupli.append(value)
         elif not value in self.dupli:
             self.unique.append(value)
-        print(self.unique, self.dupli)
+        #print("uni", self.unique)
+        #print("dup", self.dupli)
 
-    
-qu = FirstUnique([7, 7, 7, 7, 7, 7])
+
+qu = FirstUnique([7,7,7,7,7])
 qu.showFirstUnique()
 qu.add(7)
 qu.add(3)
