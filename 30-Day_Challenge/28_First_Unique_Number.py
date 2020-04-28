ff2d069a8586e63255890253d86a92476386f379
MiyabiTane <miyabitane@yahoo.co.jp>
2020-04-28 20:40:54 +0900

    WA

--- 30-Day_Challenge/28_First_Unique_Number.py
+++ 30-Day_Challenge/28_First_Unique_Number.py
@@ -3,32 +3,33 @@ class FirstUnique:
 
     def __init__(self, nums):
         self.queue = nums
-        self.unique = collections.OrderedDict()
+        self.unique = []
+        self.dupli = []
         for num in nums:
             if num in self.unique:
-                self.unique.pop(num)
+                self.unique.pop(self.unique.index(num))
+                if not num in self.dupli:
+                    self.dupli.append(num)
             else:
-                self.unique[num] = 0
-                
+                self.unique.append(num)
+
     def showFirstUnique(self):
         if self.unique:
-            item = self.unique.popitem(last = False)[0] #先頭のkey取り出し
-            self.unique[item] = 0
-            self.unique.move_to_end(item, False) #itemを先頭へ移動
-            print(item)
-            print(self.unique)
-            return item
+            print(self.unique[0])
+            return self.unique[0]
         print(-1)
         return -1
 
     def add(self, value):
         if value in self.unique:
-            self.unique.pop(value)
-        else:
-            self.unique[value] = 0
-        print(self.unique)
+            self.unique.pop(self.unique.index(value))
+            if not value in self.dupli:
+                self.dupli.append(value)
+        elif not value in self.dupli:
+            self.unique.append(value)
+        print(self.unique, self.dupli)
 
-
+    
 qu = FirstUnique([7, 7, 7, 7, 7, 7])
 qu.showFirstUnique()
 qu.add(7)
