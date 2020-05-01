5d12b17157d12218470cca35c08d9fcc9cc665ec
MiyabiTane <miyabitane@yahoo.co.jp>
2020-05-01 12:18:18 +0900

    TreeNode 応用

--- 30-Day_Challenge/30_Check_If_a_String_Is_a_Valid_Sequence_from_Root_to_Leaves_Path_in_a_Binary_Tree.py
+++ 30-Day_Challenge/30_Check_If_a_String_Is_a_Valid_Sequence_from_Root_to_Leaves_Path_in_a_Binary_Tree.py
@@ -7,31 +7,20 @@
 
 class Solution:
     def isValidSequence(self, root, arr):
-        memo = {}
-        n = len(arr)
-        def DFS(cur_node, cur_arr, arr):
-            if cur_arr == n-1:
-                if cur_node.val == arr[cur_arr]:
-                    if not cur_node.left or not cur_node.right:
-                        memo[(cur_node, cur_arr)] = 1 #True
-                    else:
-                        memo[(cur_node, cur_arr)] = 0 #False
-            elif not cur_node.left and not cur_node.right:
-                memo[(cur_node, cur_arr)] = 0
-            elif not (cur_node, cur_arr) in memo:
-                if cur_node.val == arr[cur_arr] and DFS(cur_node.left):
-                    memo[(cur_node, cur_arr)] = 1
-                else:
-                    memo[cur_node, cur_arr]
-                
-
-
-
-            if cur_arr = len(arr) - 1:
-                if cur_arr.left == arr[-1] or cur_arr.right == arr[-1]:
+        def Helper(cur_node, cur_index, arr):
+            if not cur_node:
+                if len(arr) != 0:
+                    return False
+            if cur_index == len(arr) - 1:
+                if cur_node.val == arr[cur_index] and not cur_node.left and not cur_node.right:
                     return True
-            
+            elif cur_index < len(arr) - 1 and cur_node.val == arr[cur_index]:
+                return Helper(cur_node.left, cur_index+1, arr) or Helper(cur_node.right, cur_index+1, arr)
+        return Helper(root, 0, arr)
 
+
+
+            
 
 
 
