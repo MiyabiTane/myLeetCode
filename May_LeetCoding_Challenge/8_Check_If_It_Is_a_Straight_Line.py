fee51925da0d505822f2cef0ca1633e1abb8c8ee
MiyabiTane <miyabitane@yahoo.co.jp>
2020-05-08 23:23:19 +0900

    ２次元配列の値の取り出し方

--- May_LeetCoding_Challenge/8_Check_If_It_Is_a_Straight_Line.py
+++ May_LeetCoding_Challenge/8_Check_If_It_Is_a_Straight_Line.py
@@ -2,21 +2,11 @@ import numpy as np
 def checkStraightLine(coordinates) -> bool:
     if len(coordinates) == 2:
         return True
-    coordinates = np.array(coordinates)
-    diff_x = coordinates[1, 0] - coordinates[0, 0]
-    diff_y = coordinates[1, 1] - coordinates[0, 1]
-    if diff_x == 0:
-        if len(set(coordinates[:, 1])) == 1:
-            return True
-        return False
-    a = diff_y / diff_x
-    b = coordinates[0,1] - a * coordinates[0,0]
-    #print(diff_x, diff_y)
-    for coords in coordinates[2:]:
-       if coords[1] != a * coords[0] + b:
-           return False
+    (x0,y0), (x1,y1) = coordinates[:2]
+    for x,y in coordinates:
+        if (x - x0) * (y - y1) != (x - x1) * (y - y0):
+            return False
     return True
-
 
 ans = checkStraightLine([[1, 1], [2, 2], [3, 4], [4, 5], [5, 6], [7, 7]])
 print(ans)
