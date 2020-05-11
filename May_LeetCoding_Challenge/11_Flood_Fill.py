d7ace4bf152b2ece8a65aef42110798ddb1b11e5
MiyabiTane <miyabitane@yahoo.co.jp>
2020-05-12 01:31:24 +0900

    DFS

--- May_LeetCoding_Challenge/11_Flood_Fill.py
+++ May_LeetCoding_Challenge/11_Flood_Fill.py
@@ -2,16 +2,21 @@ def floodFill(image, sr, sc, newColor):
     n = len(image)
     m = len(image[0])
     originalColor = image[sr][sc]
-    seen = [0] * (n*m)
+    #この時、出発点との間を永遠にループしてしまう
+    if originalColor == newColor:
+        return image
     def floodFillHelper(x, y):
-        seen[x + 3*y] = 1
         if image[x][y] == originalColor:
             image[x][y] = newColor
-        print(seen)
-        if not 0 in seen:
-            return image
-        else:
-            return floodFillHelper(max(x-1, 0), y), floodFillHelper(min(x+1, n-1), y), floodFillHelper(x, max(y-1, 0)), floodFillHelper(x, min(y+1, m-1))
+            if x > 0:
+                floodFillHelper(x-1, y)
+            if y > 0:
+                floodFillHelper(x, y-1)
+            if x < n-1:
+                floodFillHelper(x+1, y)
+            if y < m-1:
+                floodFillHelper(x, y+1)
+
     floodFillHelper(sr, sc) 
     return image
 
