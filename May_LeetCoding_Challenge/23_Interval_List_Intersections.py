8ce62545271d8cc36d948de7730507463febbdb1
MiyabiTane <miyabitane@yahoo.co.jp>
2020-05-24 15:36:37 +0900

    Two Pointer

--- May_LeetCoding_Challenge/23_Interval_List_Intersections.py
+++ May_LeetCoding_Challenge/23_Interval_List_Intersections.py
@@ -1,23 +1,17 @@
 def intervalIntersection(A, B):
-    leng = max(A[-1][-1], B[-1][-1])
-    how_many = [0]*(leng+1)
+    p_a = 0; p_b = 0
     ans = []
-    for a in A:
-        for i in range(a[0], a[1]+1):
-            how_many[i] += 1
-    for b in B:
-        for i in range(b[0], b[1]+1):
-            how_many[i] += 1
-    print(how_many)
-    flag = 0
-    for i in range(len(how_many)):
-        if how_many[i] == 2 and flag == 0:
-            start = i
-            flag = 1
-        if how_many[i] != 2 and flag == 1:
-            end = i-1
-            ans.append([start, end]) 
-            flag = 0
+    while p_a < len(A) and p_b < len(B):
+        start = max(A[p_a][0], B[p_b][0])
+        end = min(A[p_a][1], B[p_b][1])
+
+        if start <= end:
+            ans.append([start, end])
+        
+        if A[p_a][1] <= B[p_b][1]:
+            p_a += 1
+        else:
+            p_b += 1
     return ans
 
 
