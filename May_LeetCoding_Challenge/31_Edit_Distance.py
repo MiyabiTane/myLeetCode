a7b16eefe1a831a408f529b03466d0907c038bad
MiyabiTane <miyabitane@yahoo.co.jp>
2020-06-01 18:01:36 +0900

    DP

--- May_LeetCoding_Challenge/31_Edit_Distance.py
+++ May_LeetCoding_Challenge/31_Edit_Distance.py
@@ -1,23 +1,18 @@
 def minDistance(word1, word2):
-    n = len(word1)
-    m = len(word2)
-    if n == 0:
-        return m
-    if m == 0:
-        return n
+    n = len(word1) + 1
+    m = len(word2) + 1
     dp = [[0 for _ in range(m)] for _ in range(n)]
-    dp[0][0] = 0 if word1[0] == word2[0] else 1
 
     #１行目更新
-    for i in range(1, m):
-        dp[0][i] = dp[0][i-1] if word2[i] == word1[0] else dp[0][i-1] + 1
+    for i in range(m):
+        dp[0][i] = i
     #１列目更新
     for j in range(1, n):
-        dp[j][0] = dp[j-1][0] if word2[0] == word1[j] else dp[j-1][0] + 1
+        dp[j][0] = j
         
     for i in range(1, n):
         for j in range(1, m):
-            if word1[i] == word2[j]:
+            if word1[i-1] == word2[j-1]:
                 dp[i][j] = dp[i-1][j-1]
             else:
                 dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
