232c167631812e2f32baa3b7b4bbc6d5b37118bc
MiyabiTane <miyabitane@yahoo.co.jp>
2021-03-13 23:11:31 +0900

    update

--- Top_Interview_Questions/Trees/Maximum_Depth_of_Binary_Tree.py
+++ Top_Interview_Questions/Trees/Maximum_Depth_of_Binary_Tree.py
@@ -5,18 +5,8 @@ class TreeNode(object):
         self.right = right
 
 class Solution(object):
-    def CountDepth(self, root, count):
-        if root.left and root.right:
-            return max(self.CountDepth(root.left, count+1), self.CountDepth(root.right, count+1))
-        elif root.left:
-            return self.CountDepth(root.left, count+1)
-        elif root.right:
-            return self.CountDepth(root.right, count+1)
-        else:
-            return count
-
     def maxDepth(self, root):
         if not root:
             return 0
         else:
-            return self.CountDepth(root, 1)
+            return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
