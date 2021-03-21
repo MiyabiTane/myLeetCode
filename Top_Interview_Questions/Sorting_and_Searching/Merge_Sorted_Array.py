ff33a71c6de647b67365e9e3c0815582f5ca70ed
MiyabiTane <miyabitane@yahoo.co.jp>
2021-03-21 23:49:14 +0900

    リストの書き換え

--- Top_Interview_Questions/Sorting_and_Searching/Merge_Sorted_Array.py
+++ Top_Interview_Questions/Sorting_and_Searching/Merge_Sorted_Array.py
@@ -1,13 +1,13 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        while m > 0:
-            if n > 0  and nums1[m-1] <= nums2[n-1]:
+        while n > 0:
+            if m <= 0 or nums1[m-1] <= nums2[n-1]:
                 nums1[m+n-1] = nums2[n-1]
                 n -= 1
             else:
                 nums1[m+n-1] = nums1[m-1]
                 m -= 1
-            print(nums1)
+            # print(nums1)
 
 sol = Solution()
 sol.merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)
