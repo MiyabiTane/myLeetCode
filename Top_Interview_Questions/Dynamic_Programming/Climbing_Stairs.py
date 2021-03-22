31177776b7078ea63dccd701389135da771c8d49
MiyabiTane <miyabitane@yahoo.co.jp>
2021-03-22 20:45:00 +0900

    DP

--- Top_Interview_Questions/Dynamic_Programming/Climbing_Stairs.py
+++ Top_Interview_Questions/Dynamic_Programming/Climbing_Stairs.py
@@ -4,5 +4,7 @@ class Solution(object):
             return 1
         elif n == 2:
             return 2
-        return self.climbStairs(n-1) + self.climbStairs(n-2) 
-
+        dp = [1, 2] + [0] * (n - 2)
+        for i in range(2, n):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[-1]
